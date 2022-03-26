b495292e1e73c0df5bfe76918b640bbaba35f508
AirWav2021 <AirWav2015@gmail.com>
2022-03-26 16:52:06 +0300

    lesson 5 Feat: init project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Layout } from './components/common/Layout'
+import { Home } from './components/pages/Home'
+
+export const App = () => {
+	return (
+		<Layout>
+			<Home />
+		</Layout>
+	)
+}
